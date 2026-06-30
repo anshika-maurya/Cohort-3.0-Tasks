0383b1415f02a636e588a806e5a40fd2053a1341
anshika-maurya <anshikamaurya6400@gmail.com>
2026-06-30 13:20:00 +0530

    Personal Finance Tracker

--- FinTrack Pro/script.js
+++ FinTrack Pro/script.js
@@ -1,22 +1,20 @@
-let register = document.querySelector("#register")
-let registerBtn = document.querySelector("#register_btn")
-let h2 = document.querySelector("h2")
-let heading = document.querySelector("#heading")
-let userName = document.querySelector("label")
-let btn = document.querySelector("button")
+let register = document.querySelector("#register");
+let registerBtn = document.querySelector("#register_btn");
+let h2 = document.querySelector("h2");
+let heading = document.querySelector("#heading");
+let userName = document.querySelector("label");
+let btn = document.querySelector("button");
 
-
-if(registerBtn.textContent === "Register here"){
-    registerBtn.addEventListener("click", function(e){
-    h2.textContent = "Create Account"
-    heading.textContent = "Join FinTrack Pro"
-    userName.textContent = "Choose a Username"
-    btn.textContent = "Register"
-    btn.style.backgroundColor = "#28A745"
-    register.innerHTML = "Already have an account? <span>Login here</span>" 
-})
+if (registerBtn.textContent === "Register here") {
+  registerBtn.addEventListener("click", function (e) {
+    h2.textContent = "Create Account";
+    heading.textContent = "Join FinTrack Pro";
+    userName.textContent = "Choose a Username";
+    btn.textContent = "Register";
+    btn.style.backgroundColor = "#28A745";
+    register.innerHTML = "Already have an account? <span>Login here</span>";
+  });
 }
-
 
 //chart
 const income = 5000;
@@ -25,98 +23,93 @@ const expenses = 100;
 const ctx = document.getElementById("myChart");
 
 new Chart(ctx, {
-    type: "bar",
+  type: "bar",
 
-    data: {
-        labels: ["Income vs Expenses"],
+  data: {
+    labels: ["Income vs Expenses"],
 
-        datasets: [
-            {
-                label: "Income",
-                data: [income],
-                backgroundColor: "#166534",
-                barThickness: 230,
-                borderRadius: 2,
-                borderSkipped: false,
-                barPercentage: 0.9,
-                categoryPercentage: 0.6
-            },
-            {
-                label: "Expenses",
-                data: [expenses],
-                backgroundColor: "#b91c1c",
-                barThickness: 230,
-                borderRadius: 2,
-                borderSkipped: false,
-                barPercentage: 0.9,
-                categoryPercentage: 0.6
-            }
-        ]
+    datasets: [
+      {
+        label: "Income",
+        data: [income],
+        backgroundColor: "#166534",
+        barThickness: 230,
+        borderRadius: 2,
+        borderSkipped: false,
+        barPercentage: 0.5,
+        categoryPercentage: 0.7,
+      },
+      {
+        label: "Expenses",
+        data: [expenses],
+        backgroundColor: "#991B1B",
+        barThickness: 230,
+        borderRadius: 2,
+        borderSkipped: false,
+        barPercentage: 0.5,
+        categoryPercentage: 0.7,
+      },
+    ],
+  },
+
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+
+    plugins: {
+      legend: {
+        position: "top",
+        labels: {
+          boxWidth: 40,
+          boxHeight: 12,
+          padding: 20,
+          color: "#374151",
+          font: {
+            size: 12,
+            weight: "500",
+          },
+        },
+      },
+
+      tooltip: {
+        backgroundColor: "#111827",
+
+        callbacks: {
+          label(context) {
+            return `${context.dataset.label}: ${context.raw.toLocaleString()}`;
+          },
+        },
+      },
     },
 
-    options: {
-        responsive: true,
-        maintainAspectRatio: false,
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+        border: {
+          display: false,
+        },
+      },
 
-        plugins: {
-            legend: {
-                position: "top",
-                labels: {
-                    boxWidth: 40,
-                    boxHeight: 12,
-                    padding: 20,
-                    color: "#374151",
-                    font: {
-                        size: 12,
-                        weight: "500"
-                    }
-                }
-            },
+      y: {
+        beginAtZero: true,
 
-            tooltip: {
-                backgroundColor: "#111827",
-
-                callbacks: {
-                    label(context) {
-                        return `${context.dataset.label}: ${context.raw.toLocaleString()}`;
-                    }
-                }
-            }
+        ticks: {
+          callback(value) {
+            return value.toLocaleString();
+          },
+          color: "#6b7280",
         },
 
-        scales: {
+        grid: {
+          color: "#e5e7eb",
+        },
 
-            x: {
-                grid: {
-                    display: false
-                },
-                border: {
-                    display: false
-                }
-            },
-
-            y: {
-                beginAtZero: true,
-
-                ticks: {
-                    callback(value) {
-                        return value.toLocaleString();
-                    },
-                    color: "#6b7280",
-                    
-                },
-
-                grid: {
-                    color: "#e5e7eb",
-                    
-    
-                },
-
-                border: {
-                    display: false
-                }
-            }
-
-        }
-    }
+        border: {
+          display: false,
+        },
+      },
+    },
+  },
 });
